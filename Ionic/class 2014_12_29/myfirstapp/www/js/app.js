a4e82c4206580f33bd695928dbadde7768e73430
srehanuddin <s.rehanuddin@gmail.com>
2014-12-30 00:00:14 +0500

    Upload

--- Ionic/class 2014_12_29/myfirstapp/www/js/app.js
+++ Ionic/class 2014_12_29/myfirstapp/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,80 +23,164 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        $stateProvider
+            .state('home', {
+                url:"/home",
+                templateUrl: "templates/home.html",
+                controller: "myController"
+            })
+            .state('about', {
+                url:"/about",
+                templateUrl: "templates/about.html",
+                controller: "about"
+            })
+            .state('contact', {
+                url:"/contact",
+                templateUrl: "templates/contact.html",
+                controller: "contact"
+            })
+            .state('chat', {
+                url:"/chat",
+                templateUrl: "templates/chat.html",
+                controller: "chat"
+            })
+            .state('register', {
+                url:"/register",
+                templateUrl: "templates/register.html",
+                controller: "register"
+            });
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+        $urlRouterProvider.otherwise('/home');
 
-  // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+})
+    .controller("myController", function($scope){
+        $scope.btnClick = function(){
+            alert($scope.txt1);
         }
-      }
+
+        $scope.name = "Rehan";
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+    .controller("about", function(){
+    })
+    .controller("contact", function($scope, $firebase){
+
+        var CON = new Firebase("https://demossuet.firebaseio.com/")
+        var commentsRef = CON.child('comments');
+        var commentsSync = $firebase(commentsRef);
+
+        var commentsArray = commentsSync.$asArray();
+        $scope.comments = commentsArray;
+        //$scope.messages = [];
+
+        $scope.contact = {
+            firstname : "",
+            lastname : "",
+            comments : "",
+            time : ""
         }
-      }
+
+
+        $scope.submitComment = function(){
+            //alert($scope.message.text);
+
+            var msg = {
+                firstname : $scope.contact.firstname,
+                lastname : $scope.contact.lastname,
+                comments : $scope.contact.comments,
+                time : Date.now()
+            }
+            $scope.comments.$add(msg);
+        }
+    })
+    .controller("chat", function($scope, $firebase){
+
+        var CON = new Firebase("https://demossuet.firebaseio.com/")
+        var messageRef = CON.child('messages');
+        var messageSync = $firebase(messageRef);
+
+        var messageArray = messageSync.$asArray();
+        $scope.messages = messageArray;
+        //$scope.messages = [];
+
+        $scope.message = {
+            text : "",
+            user : "",
+            time : ""
+        }
+
+        var user = prompt("User Name");
+
+        $scope.msgSend = function(){
+            //alert($scope.message.text);
+
+            if(!$scope.message.text.trim()){
+                return;
+            }
+
+            var msg = {
+                text : $scope.message.text,
+                user : user,
+                time : Date.now()
+            }
+            $scope.messages.$add(msg);
+        }
+
     })
 
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+.controller("register", function($scope, $firebase, $firebaseAuth){
+
+    var ref = new Firebase("https://demossuet.firebaseio.com/")
+    $scope.authObj = $firebaseAuth(ref);
+
+        $scope.register = function(){
+
+            alert($scope.user.email)
+            alert($scope.user.password)
+            $scope.authObj.$createUser($scope.user.email, $scope.user.password).then(function() {
+                alert("User created successfully!");
+            })
+        }
+        $scope.login = function(){
+
+            alert($scope.user.email)
+            alert($scope.user.password)
+            $scope.authObj.$authWithPassword({
+                email: $scope.user.email,
+                password: $scope.user.password
+            }).then(function(authData) {
+                    console.log(authData);
+                    alert("Logged in as:", authData.uid);
+                }).catch(function(error) {
+                    alert("Error: " + error);
+                });
+        }
+
+        /*$scope.authObj.$createUser("my@email.com", "mypassword").then(function() {
+            console.log("User created successfully!");
+
+            return $scope.authObj.$authWithPassword({
+                email: "my@email.com",
+                password: "mypassword"
+            });
+        }).then(function(authData) {
+                console.log("Logged in as:", authData.uid);
+            }).catch(function(error) {
+                console.error("Error: ", error);
+            });*/
+
+
+
 
 });
+
+
+
+
+
+
+
+
